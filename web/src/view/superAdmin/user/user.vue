1e28a3b09a43897febde9eacc7b78e9716241754
QQ.Meng <66815109+lele0124@users.noreply.github.com>
2025-09-26 11:31:01 +0800

    用户列表页面去掉了注：右上角头像下拉可切换角色

--- web/src/view/superAdmin/user/user.vue
+++ web/src/view/superAdmin/user/user.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <warning-bar title="注：右上角头像下拉可切换角色" />
     <div class="gva-search-box">
       <el-form ref="searchForm" :inline="true" :model="searchInfo">
         <el-form-item label="用户名">
